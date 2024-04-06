70b7b3a85435626a37b1cb5aba35242baff0b232
Shivank Chhaya <shivank.chhaya@gmail.com>
2024-04-06 21:39:21 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,109 @@
+# docs
+## basics
++ whitespace seperated
++ all commands start with `:`
++ command ends with `-` if there are no paramaters
++ paramaters go like this: `:<-prm1-prm2`(no `-` needed after)
++ comments start with `*`(whitespace seperated, so each word needs to start with `*`)
+### execution
++ has dict of variables
++ has "current variable"
++ any non-commands get upsertted(more info on that later) to the current variable
++ default variable is `_`, set to an empty string
++ system variables(start  with `_`):
++ counter increases as it moves to the next command
+
+| variable | purpose/value |
+| --- | --- |
+| `_` | defualt variable |
+| `_counter` | program counter |
+| `_true` | true boolean |
+| `_false` | false boolean |
+| `_math` | enables unary math operations |
+| `_list` | empty list - used to copy into variables to create lists |
+| `_mathpi` | pi |
+| `_mathe` | e - Euler's number |
++ other than `_`, it is highly recommended that you ***do not*** change these. you might change `_counter`, for like a goto, but definitely not the others.
+### docs info
+#### parameters
++ `: name` means parameter is variable name
++ `: string` means paramter is a string
++ `: number` means paramter is a number
++ `: value` means you can use `${variablename}` to reference a variable
++ `*` means paramter is optional
++ `upsert` in the context of this documentation is either setting the specified variable to that value, or if the specified variable is a list, appending it to the list.
+
+## commands
+### `<-(*opt: value: string)`
+print. prints value of current variable, except when `opt` is specified. If `opt` is `s` then print a space and if `opt` is `n` print a newline. if `opt` is `c` then it clears the console.
+### `>-(feedvar: name)-(mode: value: string)`
+input. input prompt is value of current variable and value is upsertted to `feedvar`. `mode` can be `n` or `s`, `n` is for numbers and `s` is for string.
+### `#-(newvar: name)`
+sets current variable to `name`. if `name` doesn't already exist, then it creates it with the value of an empty string.
++ variables cannot be just a number
++ variables cannot start with `_`, `$`
+### `M-(var1: name)-(oper: value: string)-(var2: name)`
+(binary) math operation. performs `oper` on `var1` and `var2` and upserts that value to the current variable. valid values for `oper`:
+| value for `oper` | operation |
+| --- | --- |
+| + | addition |
+| ~ | subtraction |
+| * | multiplication |
+| / | division |
+| ^ | exponentiation |
+### `m-(func: value: string)-(var: name)`
+(unary) math operation. performs `func` on `var`. upserts value to current variable. valid values for `func`:
+| value for `func` | operation |
+| --- | --- |
+| sin | sine |
+| cos | cosine |
+| tan | tangent |
+| round | round number |
+| floor | round down |
+| ceil | round up |
+| ln | natural logarithm |
+### loops
+#### `(-(loopid: string)-(condvarname: name)` and `)-(loopid: string)`
+loop. `loopid` needs to be the same between both `(` and `)`, `condvarname` is name of the variable that holds the loop condition. `(` opens the loop and `)` closes it. `loopid` should be different across all loops.
+#### `(^)-(loopid: string)`
+continue in loop. returns to the start of the loop. `loopid` should be the same as the loop it's in.
+\~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+### `?-(var1: name)-(oper: value: string)-(var2: name)`
+boolean/comparison. check if `var1` is whatever `oper` is to/than `var2`, then upserts it to the current variable.
+
+Example: `:?-var1-==-var2` - checks if var1 is equal to var2.
+### ifs
+#### `?<-(var1: name)-(oper: value: string)-(var2: name)-(ifid: string)`
+if condition. similar syntax to `?`, `ifid` is the id of the condition. it should be the same across all the else ifs, and the else. `ifid` should be different across all the if-then-elses.
+#### `<?>-(var1: name)-(oper: value: string)-(var2: name)-(ifid: string)`
+else if. same syntax as `?<`. `ifid` should be the same as the corresponding if.
+#### `>?-(ifid: string)`
+else condition. `ifid` should be the same as the corresponding if and else ifs.
+#### `?|-(ifid: string)`
+end if/then/else. again, `ifid` is the same as the one in the the entire if/then/else block.
+\~~~~~~~~~~~~~~~~~~~~~~
+### `v~v-(mode: value: string)`
+type casting. turns the current variable into a number is `mode` is `n`, and string if `mode` is `s`.
+### `>=>-(newvar: name)`
+copy the value of the current variable to `newvar`. the current variable remains unchanged.
+### `` `/-``
+delete the current variable and switch to `_`
+### `XX-`
+end program
+### `??-`
+upserts random number between 0 and 1 to current variable.
+### lists
+#### `||-(index: value: number)`
+set the current variables to the value in the current variable at index `index`. So if the current variable is "hello world" and you use `:||-2`, the current variable would become "l". this works with lists as well.
+#### `[]<-(valvar: name)-(*index: value: number)`
+append the value of `valvar` to the current list at `index`. if `index` is omitted, it will add it to the end. this will crash if the current variable is not a list.
+#### `[]v^-(valvar: name)-(index: value: number)`
+replace the element at `index` with the value of `valvar`. this will crash if the current variable is not a list.
+\~~~~~~~~~~~~~~~~~~~~~
+### `{@}-`
+outputs
++ current variable
++ current variable value
++ all variables
++ loop information
++ if/then/else condition information
---
+++ main.py
@@ -0,0 +1,166 @@
+# Tasklist:
+
+import math, re, random, os, json
+math.round = round
+def lnlog(b):
+  return math.log(b)
+math.ln = lnlog
+from simpleeval import simple_eval
+f = open('main')
+sourcecode = f.read()
+f.close()
+variables = {'_': '', '_counter':0, '_true':True, '_false':False, '_math':math, '_list':[], '_math_pi':math.pi, '_math_e':math.e}
+loops = {}
+ifs = {}
+curvar = '_'
+def number(st):
+  try:
+    return int(st)
+  except ValueError:
+    return float(st)
+def seval(expr):
+  return simple_eval(expr, names=variables)
+def value(v):
+  if '${' in v:
+    return variables[v[2:-1]]
+  else:
+    try:
+      return number(v)
+    except ValueError:
+      return v
+def upsertVar(val, var):
+  global variables
+  if type(variables[var]) == list:
+    variables[var].append(val)
+  else:
+    variables[var] = val
+def emoticon2(code):
+  global curvar
+  ncode = code.split()
+  while variables['_counter'] < len(ncode):
+    inccounter = True
+    cmd = ncode[variables['_counter']]
+    if cmd.startswith(':'):
+      ncmd = cmd[1:cmd.find('-')]
+      prms = cmd[cmd.find('-'):].split('-')[1:]
+      if ncmd == '<':
+        opt = value(prms[0])
+        if opt == 's':
+          print(' ', end='')
+        elif opt == 'n':
+          print('\n', end='')
+        elif opt == 'c':
+          os.system('clear')
+        else:
+          print(variables[curvar], end='')
+      elif ncmd == '>':
+        w = {'n':number,'s':str}[value(prms[1])]
+        upsertVar(w(input(variables[curvar])), prms[0])
+      elif ncmd == '#':
+        curvar = prms[0]
+        if curvar not in variables:
+          variables[curvar] = ''
+      elif ncmd == 'M':
+        upsertVar(seval(prms[0] + value(prms[1]).replace('~','-').replace('^','**') + prms[2]), curvar)
+      elif ncmd == 'm':
+        upsertVar(seval('_math.'+value(prms[0])+'('+prms[1]+')'), curvar)
+      elif ncmd == '(':
+        loops[prms[0]] = variables['_counter']
+        if not variables[prms[1]]:
+          variables['_counter'] = ncode.index(':)-' + prms[0], variables['_counter'])
+      elif ncmd == ')' or ncmd == '(^)':
+        variables['_counter'] = loops[prms[0]]
+        inccounter = False
+      elif ncmd == '?':
+        upsertVar(seval(prms[0] + value(prms[1]) + prms[2]), curvar)
+      elif ncmd == '?<':
+        if not seval(prms[0] + value(prms[1]) + prms[2]):
+          r = re.compile(r":<\?>.*?"+prms[3])
+          nxtcondl = list(filter(r.match, ncode[variables['_counter']+1:]))
+          if len(nxtcondl) == 0:
+            if ':>?-'+prms[3] in ncode:
+              variables['_counter'] = ncode.index(':>?-' + prms[3])
+            else:
+              variables['_counter'] = ncode.index(':?|-'+prms[3])
+          elif nxtcondl[0] in ncode:
+            variables['_counter'] = ncode.index(nxtcondl[0], variables['_counter']) - 1
+          else:
+            if ':>?-'+prms[3] in ncode:
+              variables['_counter'] = ncode.index(':>?-' + prms[3])
+            else:
+              variables['_counter'] = ncode.index(':?|-'+prms[3])
+        else:
+          ifs[prms[3]] = True
+      elif ncmd == '<?>':
+        if prms[3] in ifs:
+          variables['_counter'] = ncode.index(':?|-'+prms[3])
+          continue
+        if not seval(prms[0] + value(prms[1]) + prms[2]):
+          r = re.compile(r":<\?>.*?"+prms[3])
+          nxtcondl = list(filter(r.match, ncode[variables['_counter']+1:]))
+          if len(nxtcondl) == 0:
+            if ':>?-'+prms[3] in ncode:
+              variables['_counter'] = ncode.index(':>?-' + prms[3])
+            else:
+              variables['_counter'] = ncode.index(':?|-'+prms[3])
+          elif nxtcondl[0] in ncode:
+            variables['_counter'] = ncode.index(nxtcondl[0], variables['_counter']) - 1
+          else:
+            if ':>?-'+prms[3] in ncode:
+              variables['_counter'] = ncode.index(':>?-' + prms[3])
+            else:
+              variables['_counter'] = ncode.index(':?|-'+prms[3])
+        else:
+          ifs[prms[3]] = True
+      elif ncmd == '>?':
+        if prms[0] in ifs:
+          variables['_counter'] = ncode.index(':?|-'+prms[0])
+          continue
+      elif ncmd == '?|':
+        if prms[0] in ifs:
+          del ifs[prms[0]]
+      elif ncmd == 'v~v':
+        if value(prms[0]) == 'n':
+          variables[curvar] = number(variables[curvar])
+        elif prms[0] == 's':
+          variables[curvar] = str(variables[curvar])
+      elif ncmd == '||':
+        variables[curvar] = variables[curvar][value(prms[0])]
+      elif ncmd == '>=>':
+        variables[prms[0]] = variables[curvar].copy()
+      elif ncmd == '`/':
+        del variables[curvar]
+        curvar = '_'
+      elif ncmd == 'XX':
+        return
+      elif ncmd == '??':
+        upsertVar(random.uniform(0,1), curvar)
+      elif ncmd == '[]<':
+        if len(prms) == 1:
+          variables[curvar].append(variables[prms[0]])
+        else:
+          variables[curvar].insert(value(prms[1]), variables[prms[0]])
+      elif ncmd == '[]v^':
+        variables[curvar][value(prms[1])] = variables[prms[0]]
+      elif ncmd == '{@}':
+        print('\n=>\nprogram info:')
+        print('current variable: ' + curvar)
+        print('value: ' + str(variables[curvar]))
+        vars = variables.copy()
+        vars['_math'] = str(vars['_math'])
+        print('variables: ' + json.dumps(vars,indent=2))
+        print('loops: ' + json.dumps(loops,indent=2))
+        print('ifs: ' + json.dumps(ifs,indent=2))
+      else:
+        print('\ni don\'t know what to do with a ' + cmd)
+        return
+    elif cmd.startswith('*'):
+      pass
+    else:
+      try:
+        upsertVar(number(cmd), curvar)
+      except ValueError:
+        upsertVar(cmd, curvar)
+    if inccounter:
+      variables['_counter'] += 1
+emoticon2(sourcecode)
